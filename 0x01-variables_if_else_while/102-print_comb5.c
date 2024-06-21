d89a2cad19745f6f81cf066966be886bcf549d82
Chungyoung250 <cympamyabigwi5@gmail.com>
2024-06-22 01:27:43 +0200

    yessir

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,42 +1,33 @@
-nclude <stdio.h>
+#include <stdio.h>
 
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-	    int tens1, ones1, tens2, ones2;
+int main(void) {
+	  for (int tens = 0; tens <= 9; tens++) {
+		      for (int units = tens; units <= 9; units++) {
+			            putchar(tens + '0');
+				          putchar(units + '0');
+					        if (tens != 9 || units != 9) {
+							        putchar(',');
+								        putchar(' ');
+									      }
+						    }
+		        }
+	    putchar('\n');
+	      return (0);
+}
+#include <stdio.h>
 
-	        for (tens1 = 0; tens1 <= 9; tens1++)
-			    {
-				            for (ones1 = 0; ones1 <= 9; ones1++)
-						            {
-								                for (tens2 = tens1; tens2 <= 9; tens2++)
-											            {
-													                    int start_ones = (tens1 == tens2) ? ones1 + 1 : 0;  // Start ones digit based on tens digit
-																						//
-																						//                 for (ones2 = start_ones; ones2 <= 9; ones2++)
-																						//                                 {
-																						//                                                     putchar(tens1 + '0');
-																						//                                                                         putchar(ones1 + '0');
-																						//                                                                                             putchar(' ');
-																						//                                                                                                                 putchar(tens2 + '0');
-																						//                                                                                                                                     putchar(ones2 + '0');
-																						//
-																						//                                                                                                                                                         if (!(tens1 == 9 && ones1 == 8 && tens2 == 9 && ones2 == 9))
-																						//                                                                                                                                                                             {
-																						//                                                                                                                                                                                                     putchar(',');
-																						//                                                                                                                                                                                                                             putchar(' ');
-																						//                                                                                                                                                                                                                                                 }
-																						//                                                                                                                                                                                                                                                                 }
-																						//                                                                                                                                                                                                                                                                             }
-																						//                                                                                                                                                                                                                                                                                     }
-																						//                                                                                                                                                                                                                                                                                         }
-																						//
-																						//                                                                                                                                                                                                                                                                                             putchar('\n');
-																						//
-																						//                                                                                                                                                                                                                                                                                                 return (0);
-																						//                                                                                                                                                                                                                                                                                                 }
-																						//
+int main(void) {
+	  for (int tens = 0; tens <= 9; tens++) {
+		      for (int units = tens; units <= 9; units++) {
+			            putchar(tens + '0');
+				          putchar(units + '0');
+					        if (tens != 9 || units != 9) {
+							        putchar(',');
+								        putchar(' ');
+									      }
+						    }
+		        }
+	    putchar('\n');
+	      return (0);
+}
+
