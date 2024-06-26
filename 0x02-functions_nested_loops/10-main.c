c9cf0aaec6c656aca3fbd05a92918397a1f1cb8c
Chungyoung250 <140666155+Chungyoung250@users.noreply.github.com>
2024-06-27 01:17:47 +0200

    Update 10-main.c

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * add - Check Main
- * @a: An integer a
- * @b: An integer b
- * Description: Function that adds two integers and returns the result
- * Return: Result to add a and b
+ * main - checks the code
+ *
+ * Return: Always 0.
  */
-int add(int a, int b)
+int main(void)
 {
-	return (a + b);
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
 }
