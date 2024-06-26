db397068b42578c95c062dcfeee3ebf966ab74b3
Chungyoung250 <140666155+Chungyoung250@users.noreply.github.com>
2024-06-27 01:24:54 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,53 +1,42 @@
 #include <stdio.h>
-
-void print_fibonacci(int n);
-
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: ALways 0 (Success)
+ */
 int main(void)
 {
-	    print_fibonacci(98);
-	        return (0);
+	unsigned long int i, j, k, j1, j2, k1, k2;
+
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
+	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
+
+	return (0);
 }
-
-/**
- *  * print_fibonacci - Prints the first n Fibonacci numbers
- *   * @n: The number of Fibonacci numbers to print
- *    */
-void print_fibonacci(int n)
-{
-	    unsigned long a_low = 1, a_high = 0;
-	        unsigned long b_low = 2, b_high = 0;
-		    unsigned long tmp_low, tmp_high;
-		        int count;
-
-			    printf("%lu, %lu", a_low, b_low);
-
-			        for (count = 2; count < n; count++)
-					    {
-						            tmp_low = a_low + b_low;
-							            tmp_high = a_high + b_high;
-
-								            if (tmp_low > 999999999)
-										            {
-												                tmp_low -= 1000000000;
-														            tmp_high += 1;
-															            }
-
-									            a_low = b_low;
-										            a_high = b_high;
-											            b_low = tmp_low;
-												            b_high = tmp_high;
-
-													            if (b_high)
-															                printf(", %lu%09lu", b_high, b_low);
-														            else
-																                printf(", %lu", b_low);
-															        }
-
-				    printf("\n");
-}
-
