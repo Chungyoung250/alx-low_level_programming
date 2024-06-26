dac65297f4f2656adffb22317658f6edf77b19a5
Chungyoung250 <cympamyabigwi5@gmail.com>
2024-06-26 18:19:51 +0200

    go

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,22 @@
 #include "main.h"
 
-void print_alphabet_x10(void) {
-    char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-    int i, j;
+/**
+ *  * print_alphabet_x10 - Prints the alphabet 10 times, in lowercase,
+ *   * followed by a new line.
+ *    */
+void print_alphabet_x10(void)
+{
+	    char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+	        int i;
 
-    for (j = 0; j < 10; j++) {
-        for (i = 0; alphabet[i] != '\0'; i++) {
-            _putchar(alphabet[i]);
-        }
-    }
+		    for (i = 0; i < 10; i++)
+			        {
+					        char *ptr = alphabet;
+						        while (*ptr)
+								        {
+										            _putchar(*ptr);
+											                ptr++;
+													        }
+							    }
 }
 
